e28f03210c891afbcbce4b569e89fd42e46e9a5e
joshwmay <45636119+joshwmay@users.noreply.github.com>
2019-11-25 06:51:45 -0500

    Pencil
    More Pencil In features implemented (touchevents to be handled later)

--- public/js/pencil.js
+++ public/js/pencil.js
@@ -3,17 +3,21 @@ window.addEventListener('load', () => {
   const ctx = canvas.getContext("2d");
   canvas.height = (250);
   canvas.width = (250);
+  canvas.style.backgroundImage = "url('img/circle.png')";
   ctx.strokeStyle = "grey";
   ctx.strokeRect(0, 0, 250,250);
+  ctx.stroke();
   function start() {
     painting = true;
   }
+
   function end() {
     painting = false;
   }
+
   function draw(e) {
     if(!painting) return;
-    ctx.lineWidth = 9;
+    ctx.lineWidth = 20;
     ctx.lineTo(e.clientX, e.clientY);
     ctx.stroke();
     ctx.beginPath();
@@ -24,5 +28,12 @@ window.addEventListener('load', () => {
   canvas.addEventListener('mousemove', draw);
 
 });
-var can = document.getElementById("pencil");
-can.style.backgroundImage = "url('img/circle.png')";
+function reset() {
+  const canvas = document.querySelector("#pencil");
+  const ctx = canvas.getContext("2d");
+  canvas.height = (250);
+  canvas.width = (250);
+  canvas.style.backgroundImage = "url('img/circle.png')";
+  ctx.strokeStyle = "grey";
+  ctx.strokeRect(0, 0, 250,250);
+};
