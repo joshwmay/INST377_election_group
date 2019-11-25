01ef84f4f10f80d007ade7eebba911eda0bc9973
joshwmay <45636119+joshwmay@users.noreply.github.com>
2019-11-25 17:08:07 -0500

    Pencil Dynamic Scripting
    Scripting added to track fill volume of canvas used in pencil and change depending on state of canvas.
    
    New images added for canvas background depending on fill volume

--- public/js/pencil.js
+++ public/js/pencil.js
@@ -1,8 +1,14 @@
 window.addEventListener('load', () => {
   const canvas = document.querySelector("#pencil");
   const ctx = canvas.getContext("2d");
-  canvas.height = (250);
-  canvas.width = (250);
+  const scale = window.devicePixelRatio;
+  canvas.height = 250;
+  canvas.width = 250;
+  var h, w, area, area_updates;
+  h = canvas.height;
+  w = canvas.width;
+  area = (h * w);
+  area_updates = 0;
   canvas.style.backgroundImage = "url('img/circle.png')";
   ctx.strokeStyle = "grey";
   ctx.strokeRect(0, 0, 250,250);
@@ -13,10 +19,12 @@ window.addEventListener('load', () => {
 
   function end() {
     painting = false;
+    updateArea();
   }
   function draw(e) {
     if(!painting) return;
     ctx.lineWidth = 20;
+    ctx.lineCap = "round";
     ctx.lineTo(e.clientX, e.clientY);
     ctx.stroke();
     ctx.beginPath();
@@ -25,11 +33,27 @@ window.addEventListener('load', () => {
   function draw_(e) {
     if(!painting) return;
     ctx.lineWidth = 20;
+    ctx.lineCap = "round";
     ctx.lineTo(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
     e.preventDefault();
+  }
+  function updateArea() {
+    var data = ctx.getImageData(0, 0, 250, 250);
+    data = data.data;
+    for (let t=0, i=0, len=data.length; i<len; i+=10) {
+      if (data[i] > 127) {
+        t += 1;
+        var out = (t / (250 * 250));
+        if(out > .17 && out < .2) {
+          canvas.style.backgroundImage = "url('img/green_circle.png')";
+        } else if (out > .2) {
+          canvas.style.backgroundImage = "url('img/red_circle.png')";
+        }
+      }
+    }
   }
   canvas.addEventListener('mousedown', start, false);
   canvas.addEventListener('mouseup', end, false);
