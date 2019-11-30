5b51ad36c28b51bdb74b026e42b46c3396a84ca5
joshwmay <45636119+joshwmay@users.noreply.github.com>
2019-11-30 16:21:35 -0500

    Edited JSON
    Removed unneeded JSON files and ballot measure characteristics

--- public/js/script.js
+++ public/js/script.js
@@ -11,24 +11,18 @@ var countDownDate = new Date("Dec 16, 2019 00:00:00").getTime();
 
 // set refresh interval of count
 var x = setInterval(function() {
-
   // get current time
   var now = new Date().getTime();
-
   // calculate time left
   var timeleft = countDownDate - now;
-
   // convert time left to days, hours, minutes, seconds
   var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
   var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-
-  // Append time to demo
+  // Append time to clock
   document.getElementById("clock").innerHTML = days + " Days" + "<br>" + hours + " Hours" + "<br>"
   + minutes + " Minutes" + "<br>" + seconds + " seconds";
-
-
 }, 1000);
 
 // Script to open and close sidebar
