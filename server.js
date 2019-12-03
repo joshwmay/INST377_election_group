b69287fd10f04aed6993205047d2a11f9b821ebd
joshwmay <45636119+joshwmay@users.noreply.github.com>
2019-12-02 22:52:39 -0500

    Removed unused lines
    Removed unused requirements

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const fetch = require('node-fetch');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
