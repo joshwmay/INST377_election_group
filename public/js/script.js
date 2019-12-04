5418eb91a3f14b65ee2f303ba61999bb3b27c237
jmay1 <jmay1@terpmail.umd.edu>
2019-12-04 12:57:45 -0500

    Added event listeners
    To alternate between sorting table in absence of server to sorting table with server (localstorage workaround?)

--- public/js/script.js
+++ public/js/script.js
@@ -602,3 +602,31 @@ window.addEventListener("load", function () {
   });
   sendData();
 });
+
+window.addEventListener("offline", () => {
+    var col0, col1, col2, col3, tbody;
+    col0 = document.getElementsByTagName("th")[0];
+    col1 = document.getElementsByTagName("th")[1];
+    col2 = document.getElementsByTagName("th")[2];
+    col3 = document.getElementsByTagName("th")[3];
+    tbody = document.getElementById("body");
+    tbody.script = "";
+    col0.onclick = sort_by_column(1);
+    col1.onclick = sort_by_column(2);
+    col2.onclick = sort_by_column(3);
+    col3.onclick = sort_by_column(4);
+});
+
+window.addEventListener("online", () => {
+    var col0, col1, col2, col3, tbody;
+    col0 = document.getElementsByTagName("th")[0];
+    col1 = document.getElementsByTagName("th")[1];
+    col2 = document.getElementsByTagName("th")[2];
+    col3 = document.getElementsByTagName("th")[3];
+    tbody = document.getElementById("body");
+    tbody.script = JSONtable(stringer("/s1"));
+    col0.onclick = JSONtable(stringer("/s1"));
+    col1.onclick = JSONtable(stringer("/s3"));
+    col2.onclick = JSONtable(stringer("/s5"));
+    col3.onclick = JSONtable(stringer("/s7"));
+})
