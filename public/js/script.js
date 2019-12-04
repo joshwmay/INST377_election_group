7f5904a61ed2c9b4e021b22aa1afe492c92b2573
joshwmay <45636119+joshwmay@users.noreply.github.com>
2019-12-03 21:56:16 -0500

    Reintroduced sort_by_column
    Possibly to be used in the event server changes do not effectively address making content usable offline

--- public/js/script.js
+++ public/js/script.js
@@ -494,6 +494,44 @@ function loadData(str) {
     })
   })
 };
+function sort_by_column(n) {
+  var table = document.getElementById("table");
+  var rows = table.rows;
+  var switching = true;
+  var switchcount = 0;
+  var dir = "asc";
+  while(switching == true) {
+    switching = false;
+    for (i = 1; i < (rows.length - 1); i += 1) {
+      var shouldSwitch = false;
+      var x = rows[i].getElementsByTagName("td")[n];
+      var y = rows[i + 1].getElementsByTagName("td")[n];
+      if (dir === "asc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      else if (dir === "desc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      switchcount += 1;
+    } else {
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+
 var countDownDate = new Date("Dec 16, 2019 00:00:00").getTime();
 
 // set refresh interval of count
